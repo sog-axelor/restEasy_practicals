418ca9514e308a25ac8e11045dfa936056dae3a6
Sonu <s.gupta@axelor.com>
2023-01-20 15:23:55 +0530

    Some Annotation Method

--- src/main/java/com/example/rest/Test.java
+++ src/main/java/com/example/rest/Test.java
@@ -1,21 +1,80 @@
 package com.example.rest;
 
-import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/test")
 public class Test {
 	
-	@Path("/hello")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String  hello()
-	{
-		System.out.println("Hello WOrld");
-		return "Hello World";
-	}
+	
+//	@Path("/hello/{Value}")				//@Pathparam
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	//@Consumes(MediaType.APPLICATION_JSON)
+//	public String  hello(@PathParam("Value") String Value)
+//	{
+//		System.out.println("Hello WOrld" + Value);
+//		return "Hello World " + Value;
+//	}
+	
+	
+	
+//	@Path("/hello")				//2.@QueryParam
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public String  hello(@QueryParam("Value") String Value)
+//	{
+//		System.out.println("Hello WOrld" + Value);
+//		return "Hello World " + Value;
+//	}
+//	
+	
+//	@Path("/hello")				//3.@MatrixParam
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public String  hello(@MatrixParam("Value") String Value)
+//	{
+//		System.out.println("Hello WOrld" + Value);
+//		return " Hello World " + Value;
+//	}
+	
+	
+//	@Path("/hello")				//3.@HeaderParam
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public String  hello(@HeaderParam("Value") String Value)
+//	{
+//		System.out.println("Hello WOrld" + Value);
+//		return " Hello World " + Value;
+//	}
+	
+//	@Path("/hello")				//3.@FormPAram
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public String  hello(@FormParam("Value") String Value)
+//	{
+//		Value = "Test";
+//		System.out.println("Hello WOrld" + Value);
+//		return " Hello World " + Value;
+//	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
