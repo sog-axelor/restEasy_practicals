0e957ff0634a5d0e1625b0eaf639b66e5b54c345
Sonu <s.gupta@axelor.com>
2023-01-20 16:04:58 +0530

    Error

--- src/main/java/com/example/rest/TestApplication.java
+++ src/main/java/com/example/rest/TestApplication.java
@@ -3,14 +3,16 @@ package com.example.rest;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@ApplicationPath("/restApi")
 public class TestApplication extends Application{
 
 	private Set<Object> singletons = new HashSet<Object>();
 	
 	public TestApplication () {
-	singletons.add(new Test());
+	this.singletons.add(new Test());
 	}
 	
 		@Override
